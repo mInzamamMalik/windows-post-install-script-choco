4a472d9d8783a6084cb064f062ad19649ac046df
Mohammad Inzamam Malik <malikasinger1@gmail.com>
2018-11-13 17:26:45 +0500

    -y

--- command.bat
+++ command.bat
@@ -3,24 +3,24 @@ REM install chocolaty
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 
-choco install adobereader
+choco install adobereader -y
 
-choco install googlechrome
-choco install winrar
-choco install vlc
-choco install git.install
-choco install putty.install
-choco install nodejs.install
-choco install skype
-choco install ccleaner
-choco install dropbox
-choco install python
-choco install awscli
-choco install vscode
-choco install smartgit
-choco install internet-download-manager
-choco install postman
-choco install docker-for-windows
-choco install docker-kitematic
-choco install qbittorrent
+choco install googlechrome -y
+choco install winrar -y
+choco install vlc -y
+choco install git.install -y
+choco install putty.install -y
+choco install nodejs.install -y
+choco install skype -y
+choco install ccleaner -y
+choco install dropbox -y
+choco install python -y
+choco install awscli -y
+choco install vscode -y
+choco install smartgit -y
+choco install internet-download-manager -y
+choco install postman -y
+choco install docker-for-windows -y
+choco install docker-kitematic -y
+choco install qbittorrent -y
 
